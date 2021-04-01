065cfe3d4616c46519ce5f043cce58b06c015907
Theepak1 <theepakkprakash@gmail.com>
2021-04-01 12:56:06 +0530

    basic structure push 1

---
+++ OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/bean/User.java
@@ -0,0 +1,7 @@
+package com.capg.onlinesportsshopee.bean;
+
+public class User {
+	private String userId;
+	private String password;
+	private String role;
+}
---
+++ OnlineSportsShopee/src/main/java/com/capg/onlinesportsshopee/model/Payment.java
@@ -0,0 +1,8 @@
+package com.capg.onlinesportsshopee.model;
+
+public class Payment {
+	private long paymentId;
+	private String type;
+	private String status;
+	private Card card;
+}
